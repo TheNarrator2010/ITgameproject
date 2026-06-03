24b823f6b1e53cdc75f73dd73045362151e0549d
TheNarrator2010 <witold.waszczuk@gmail.com>
2026-06-03 11:43:43 +0200

    Dzisiaj robilem 2 voidy ktorymi opisywalem opiswyanie sasiadow i liczenie czegostam

--- MINESWEEPER.cpp
+++ MINESWEEPER.cpp
@@ -1,13 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
+int wierszgracza,kolumnagracza;
+const int wiersze=10;
+const int kolumny=10;
+const int logicznamina=-1;
+char planszagracza[wiersze][kolumny];
+int planszalogiczna[wiersze][kolumny];
+    void obliczanieprzylegajacych(int planszalogiczna[wiersze][kolumny]){
+        for(int r=0;r<wiersze;r++){
+            for(int c=0;c<kolumny;c++)
+        if(planszalogiczna[r][c]==logicznamina){
+            for(int i=-1;i<=1;i++){
+                for(int j=-1;j<=1;j++){
+                    int nowyr=r+i;
+                    int nowyc=c+j;
+
+                }
+            }
+        }
+           }
+    }
+    void odkrywaniePol(int w, int k,int planszalogiczna[wiersze][kolumny], char planszagracza[wiersze][kolumny]){
+        //zabezpieczenie 1-pozaplanszowcy
+        if(wierszgracza < 0 || wierszgracza>=wiersze || kolumnagracza<0 || kolumnagracza>=kolumny) return;
+        //zabezpieczenie 2-juz odkryte pole
+        if(planszagracza[wierszgracza][kolumnagracza]!='-') return;
+        //odkrywanie
+        planszagracza[wierszgracza][kolumnagracza] = planszalogiczna[wierszgracza][kolumnagracza] + '0';
+    if(planszalogiczna[wierszgracza][kolumnagracza]==0){
+        for(int i = -1;i<=1;i++){
+            for(int j=-1;j<=1;j++){
+                odkrywaniePol(wierszgracza+i,kolumnagracza + j,planszalogiczna,planszagracza);
+            }
+        }
+    }
+    }
 int main()
 {
 srand(time(NULL));
 const int wiersze=10;
 const int kolumny=10;
 int liczbamin=15;
-char planszagracza[wiersze][kolumny];
-int planszalogiczna[wiersze][kolumny];
 
 for(int w=0;w<wiersze;w++){
     for(int k=0;k<kolumny;k++){
@@ -26,10 +59,10 @@ while (ile_min<15){
     }
 
 }
-int x,y;
+
 while(true){
 //------------------ rys planszy
-cout <<"  ";
+cout <<"   ";
 for(int k=0;k<kolumny;k++)
     cout << k+1<<" ";
 cout <<endl;
@@ -45,14 +78,12 @@ for(int w=0;w<wiersze;w++){
 //---------------- koniec planszy
 //gracz i jego dzialania
 cout <<"Podaj wierz i kolumne:";
-cin >> x >>y;
-if(planszalogiczna[x][y]=1){
-    planszagracza[x][y]='#';
-    cout << "PRZEGRALES";
-    return(0);
+cin >> wierszgracza >> kolumnagracza;
+if(planszalogiczna[wierszgracza][kolumnagracza]=1){
+    planszagracza[wierszgracza][kolumnagracza]='#';
 
 }
 
 }
 }
-}
+
